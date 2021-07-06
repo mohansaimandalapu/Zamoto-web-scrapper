6c3f96bd406038bf45d7a1bec78136cf0fd9768a
mohan sai mandalapu <mohansaimandalapu@gmail.com>
2021-07-06 10:58:47 +0530

    Updates in streamlit

--- task-2-Data_cleaning/sentiment_analysis_streamlit.py
+++ task-2-Data_cleaning/sentiment_analysis_streamlit.py
@@ -2,31 +2,6 @@ import streamlit as st
 from transformers import pipeline
 from pathlib import Path
 
-import base64
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('P.jpg')
-
-
 
 st.markdown('<h1 align="center"> RESTAURENT REVIEW SENTIMET ANLYSIS </h1>',unsafe_allow_html=True)
 Feedback = st.text_input("enter review",None)
